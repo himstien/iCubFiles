7f32349950d9dcca3cbdfd2c86f7ee44e69ef03d
Himanshu Akolkar <himanshu.akolkar@gmail.com>
2014-07-05 16:59:15 +0200

    receptive field.cpp addded

---
+++ applyReceptiveField/src/receptiveField.cpp
@@ -0,0 +1,35 @@
+// Author: Himanshu Akolkar
+
+// Code for getting receptive fields and applying them on iCub with DVS input.
+
+
+#include "receptiveField.h"
+
+using namespace yarp::os;
+
+receptiveField::receptiveField()
+{
+
+// Initializing with parameters for a typical pyramidal cell
+size = 32;
+weightVector.resize(size*size);
+    //ctor
+}
+
+receptiveField::~receptiveField()
+{
+    //dtor
+}
+
+void receptiveField::setWeights(string filename, vector weightvector, int fieldSize)
+{
+	File fid;
+	fid.open(filename.c_str(), 'r');
+	
+	for(int n=0; n < fieldSize*fieldSize; n++)
+	{
+		weightVector[n] = fscanf(fid, %d, 1);
+	}
+}
+
+
