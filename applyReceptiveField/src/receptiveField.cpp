c07533414ad4c9488efeb986756dc33df2d727fc
icub <icub@icub14.icub.local>
2014-08-02 12:34:46 +0200

    Receptive field class for eventbased computation

--- applyReceptiveField/src/receptiveField.cpp
+++ applyReceptiveField/src/receptiveField.cpp
@@ -12,8 +12,13 @@ receptiveField::receptiveField()
 {
 
 // Initializing with parameters for a typical pyramidal cell
-size = 32;
+//size = 32;
+
 weightVector.resize(size*size);
+fieldSizeX = 32;
+fieldSizeY = 32;
+
+
     //ctor
 }
 
@@ -22,15 +27,22 @@ receptiveField::~receptiveField()
     //dtor
 }
 
-void receptiveField::setWeights(std::string filename, yarp::sig::Vector weightvector, int fieldSize)
+void receptiveField::setWeights(std::string filename, yarp::sig::Vector weightvector)
 {
 	std::ifstream fid;
 	fid.open(filename.c_str());
 	
-	for(int n=0; n < fieldSize*fieldSize; n++)
+	for(int n=0; n < fieldSizeX*fieldSizeY; n++)
 	{
 		fid >> weightVector[n];
 	}
+}
+
+void receptiveField::getWeightAt(int x, int y)
+{
+    if(x > fieldSizeX || y > fieldSizeY)
+    {
+    }	
 }
 
 
