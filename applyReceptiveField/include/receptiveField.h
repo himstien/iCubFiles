cd39a69e54c88f417d019d925f2dd3a9a78948c4
icub <icub@icub14.icub.local>
2014-08-04 12:31:12 +0200

    added more functions to receptive field.cpp

--- applyReceptiveField/include/receptiveField.h
+++ applyReceptiveField/include/receptiveField.h
@@ -1,29 +1,41 @@
 #ifndef RECEPTIVEFIELD_H
 #define RECEPTIVEFIELD_H
-
+ 
 #include <yarp/os/all.h>
 #include <yarp/sig/all.h>
-
+ 
 class receptiveField
 {
     public:
         receptiveField();
         virtual ~receptiveField();
-        
-        void setWeights(std::string, yarp::sig::Vector, int);
-        bool setFieldSize(int);
-
-        yarp::sig::Vector getWeights();
-	int getFieldSize();
-	
+         
+        bool setFieldSize(int, int);
+        bool setWeightsFile(std::string);
+         
+         
+        int getFieldSizeX();
+        int getFieldSizeY();
+         
+        double getWeightAt(int, int);
+        void setWeights();
+         
+        bool normalizeWeights();
+        bool multiplyWeights();
+        bool scaleUpWeights();
+         
+        bool isValid();
+     
     protected:
         double inhibitor;
-
+ 
+ 
     private:
-        int size;
-	    yarp::sig::Vector weightVector;
+        int fieldSizeX;
+        int fieldSizeY;
+        yarp::sig::Vector weightVector;
+        bool valid;
 };
-
+ 
 #endif // RECEPTIVEFIELD_H
-
 
--- applyReceptiveField/src/receptiveField.cpp
+++ applyReceptiveField/src/receptiveField.cpp
@@ -1,48 +1,107 @@
+
 // Author: Himanshu Akolkar
-
+ 
 // Code for getting receptive fields and applying them on iCub with DVS input.
-
-
+ 
+ 
 #include "receptiveField.h"
 #include <fstream>
-
+ 
 using namespace yarp::os;
-
+ 
 receptiveField::receptiveField()
 {
-
+ 
 // Initializing with parameters for a typical pyramidal cell
 //size = 32;
-
-weightVector.resize(size*size);
 fieldSizeX = 32;
 fieldSizeY = 32;
-
-
+valid = true;
+weightVector.resize(fieldSizeX*fieldSizeY);
+ 
+ 
     //ctor
 }
-
+ 
 receptiveField::~receptiveField()
 {
     //dtor
 }
-
-void receptiveField::setWeights(std::string filename, yarp::sig::Vector weightvector)
+ 
+bool receptiveField::setFieldSize(int x, int y)
 {
-	std::ifstream fid;
-	fid.open(filename.c_str());
-	
-	for(int n=0; n < fieldSizeX*fieldSizeY; n++)
-	{
-		fid >> weightVector[n];
-	}
+    fieldSizeX = x;
+    fieldSizeY = y;
+    return true;   
 }
-
-void receptiveField::getWeightAt(int x, int y)
+ 
+int receptiveField::getFieldSizeX()
+{
+    return fieldSizeX;
+}
+ 
+int receptiveField::getFieldSizeY()
+{
+    return fieldSizeY;
+}
+ 
+ bool receptiveField::normalizeWeights()
+ {
+    double sum = 0;
+    for(int n=0; n < fieldSizeX*fieldSizeY; n++)
+    {
+        sum+=weightVector[n];
+    }
+     
+     
+    for(int n=0; n < fieldSizeX*fieldSizeY; n++)
+    {
+        weightVector[n] /= sum ;
+    }
+    return true;
+     
+ }
+ 
+ bool receptiveField::multiplyWeights( double factor)
+ {
+     
+    for(int n=0; n < fieldSizeX*fieldSizeY; n++)
+    {
+        weightVector[n] *= factor ;
+    }
+     
+ }
+   
+bool receptiveField::isValid()
+{
+    return valid;
+}
+ 
+         
+void receptiveField::setWeights()
+{
+    std::ifstream fid;
+    fid.open(weightsFile.c_str());
+     
+    for(int n=0; n < fieldSizeX*fieldSizeY; n++)
+    {
+        fid >> weightVector[n];
+    }
+}
+ 
+bool receptiveField::setWeightsFile(std::string filename)
+{
+    weightsFile = filename;
+    valid = true;
+    return true;
+}
+ 
+double receptiveField::getWeightAt(int x, int y)
 {
     if(x > fieldSizeX || y > fieldSizeY)
     {
-    }	
+        return -1.0;
+    }  
+    return ( weightVector.at((x-1)*(fieldSizeX)+ y));
 }
-
 
