8a2b821539ff5f50ddf9ecf69ba711d901eb6a62
icub <icub@icub14.icub.local>
2014-09-12 04:56:20 +0200

    .

---
+++ applyReceptiveField/include/yarpDefault.h
@@ -0,0 +1,23 @@
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Thread.h>
+#include <yarp/os/Time.h>
+#include <yarp/sig/Vector.h>
+#include <yarp/sig/Image.h>
+
+#include <iCub/emorph/eventBottle.h>
+#include <iCub/emorph/eventCodec.h>
+
+class yarpDefault
+{
+    public:
+        yarpDefault();
+        ~yarpDefault();
+        yarp::os::BufferedPort<yarp::os::Bottle> inputPort;    
+        yarp::sig::ImageOf<yarp::sig::PixelMono> imageToWrite;
+        
+        yarp::os::Port imagePort;    
+        
+//        yarp::os::BufferedPort<yarp::os::Bottle> bufferPort;
+
+};
